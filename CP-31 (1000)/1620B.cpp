166d9b76790c43c3c1beea72bedbc5f9315dfa7a
shikherjha <jhasahab1906@gmail.com>
2025-09-07 15:25:10 +0530

    CP-31 sheet and contest updates

---
+++ CP-31 (1000)/1620B.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define f(i,a,b) for(int i=a; i<b; i++)
+#define rf(i,a,b) for(int i=a; i>=b; i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define dbg(x)
+#endif
+
+template<class T> void _print(T x) { cerr << x; }
+template<class T, class V> void _print(pair<T,V> p) { cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}"; }
+template<class T> void _print(vector<T> v) { cerr << "["; for (auto i : v) { _print(i); cerr << " "; } cerr << "]"; }
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+int mod_add(int a, int b) { return (a % MOD + b % MOD) % MOD; }
+int mod_sub(int a, int b) { return (a % MOD - b % MOD + MOD) % MOD; }
+int mod_mul(int a, int b) { return (a % MOD * b % MOD) % MOD; }
+int mod_pow(int a, int b) {
+    int res = 1;
+    a %= MOD;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a);
+        a = mod_mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+int mod_inv(int a) { return mod_pow(a, MOD - 2); }
+
+struct DSU {
+    vi parent, rank;
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        f(i, 0, n) parent[i] = i;
+    }
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    bool unite(int x, int y) {
+        int xr = find(x), yr = find(y);
+        if (xr == yr) return false;
+        if (rank[xr] < rank[yr]) swap(xr, yr);
+        parent[yr] = xr;
+        if (rank[xr] == rank[yr]) rank[xr]++;
+        return true;
+    }
+};
+
+void dfs(int node, vi adj[], vi &vis) {
+    vis[node] = 1;
+    for (int child : adj[node]) {
+        if (!vis[child]) dfs(child, adj, vis);
+    }
+}
+void bfs(int start, vi adj[], vi &vis) {
+    queue<int> q;
+    q.push(start);
+    vis[start] = 1;
+    while (!q.empty()) {
+        int node = q.front(); q.pop();
+        for (int child : adj[node]) {
+            if (!vis[child]) {
+                vis[child] = 1;
+                q.push(child);
+            }
+        }
+    }
+}
+
+vi sieve(int n) {
+    vi isPrime(n+1, 1);
+    isPrime[0] = isPrime[1] = 0;
+    for (int i = 2; i*i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i*i; j <= n; j += i) isPrime[j] = 0;
+        }
+    }
+    return isPrime;
+}
+
+
+void solve() {
+    int w,h ;
+    cin >> w >> h;
+    
+    vector<int> a,b,c,d;
+    int x1,x2,x3,x4,y1,y2,y3,y4 ;
+    for(int i=0;i<4;i++){
+        if(i==0){
+            int k;
+            cin >> k;
+            
+            for(int j=0;j<k;j++){
+                int temp;
+                cin >> temp;
+                a.push_back(temp);
+            }
+            x1 = a[0], x2 = a[k-1];
+        }else if(i==1){
+            int k;
+            cin >> k;
+            
+            for(int j=0;j<k;j++){
+                int temp;
+                cin >> temp;
+                b.push_back(temp);
+            }
+            x3 = b[0], x4=b[k-1];
+        }else if(i==2){
+            int k;
+            cin >> k;
+            
+            for(int j=0;j<k;j++){
+                int temp;
+                cin >> temp;
+                c.push_back(temp);
+            }
+            y1 = c[0], y2 = c[k-1];
+        }else{
+            int k;
+            cin >> k;
+            
+            for(int j=0;j<k;j++){
+                int temp;
+                cin >> temp;
+                d.push_back(temp);
+            }
+            y3 = d[0], y4=d[k-1];
+        }
+
+    }
+    //case-1 (height = h)
+    int area_1 = max(x2-x1,x4-x3)*h;
+    int area_2 = max(y2-y1,y4-y3)*w; //case-2 (height=w)
+
+    int ans = max(area_1,area_2);
+
+    cout << ans << endl;
+
+}
+
+
+int32_t main() {
+    fastio();
+
+#ifdef LOCAL
+    freopen("inputf.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
---
+++ TLE eliminators/ArrayDivision.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define f(i,a,b) for(int i=a; i<b; i++)
+#define rf(i,a,b) for(int i=a; i>=b; i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define dbg(x)
+#endif
+
+template<class T> void _print(T x) { cerr << x; }
+template<class T, class V> void _print(pair<T,V> p) { cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}"; }
+template<class T> void _print(vector<T> v) { cerr << "["; for (auto i : v) { _print(i); cerr << " "; } cerr << "]"; }
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+int mod_add(int a, int b) { return (a % MOD + b % MOD) % MOD; }
+int mod_sub(int a, int b) { return (a % MOD - b % MOD + MOD) % MOD; }
+int mod_mul(int a, int b) { return (a % MOD * b % MOD) % MOD; }
+int mod_pow(int a, int b) {
+    int res = 1;
+    a %= MOD;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a);
+        a = mod_mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+int mod_inv(int a) { return mod_pow(a, MOD - 2); }
+
+
+
+void dfs(int node, vi adj[], vi &vis) {
+    vis[node] = 1;
+    for (int child : adj[node]) {
+        if (!vis[child]) dfs(child, adj, vis);
+    }
+}
+void bfs(int start, vi adj[], vi &vis) {
+    queue<int> q;
+    q.push(start);
+    vis[start] = 1;
+    while (!q.empty()) {
+        int node = q.front(); q.pop();
+        for (int child : adj[node]) {
+            if (!vis[child]) {
+                vis[child] = 1;
+                q.push(child);
+            }
+        }
+    }
+}
+
+vi sieve(int n) {
+    vi isPrime(n+1, 1);
+    isPrime[0] = isPrime[1] = 0;
+    for (int i = 2; i*i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i*i; j <= n; j += i) isPrime[j] = 0;
+        }
+    }
+    return isPrime;
+}
+
+bool helper(vi a, int sum, int k){
+    int cnt = 1;
+    int temp = 0;
+    
+    for(int i=0;i<sz(a);i++){
+        if(a[i]>sum){
+          return false;  
+        }
+        if(temp+a[i]<=sum){
+            temp+=a[i];
+        }
+        else{
+            cnt += 1;
+            temp = a[i];
+        }
+        
+    }
+    return cnt<=k;
+}
+
+
+void solve() {
+    int n,k;
+	cin >> n >> k;
+	vi a(n);
+	int sum = 0;
+	for(int i=0;i<n;i++){
+	    cin >> a[i];
+	    sum += a[i];
+	}
+
+	int s=0,e=sum,ans=-1;
+	while(s<=e){
+	    int m = s+(e-s)/2;
+	    if(helper(a,m,k)){
+	        ans = m;
+	        e = m-1;
+	        
+	    }
+	    else{
+	        s = m+1;
+	    }
+	    
+	}
+	cout << ans ;
+}
+
+int32_t main() {
+    fastio();
+    
+#ifdef LOCAL
+    freopen("inputf.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
